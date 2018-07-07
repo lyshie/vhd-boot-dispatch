1cc0335f999f4f4f33457e26cf9ed99fac0fa5ea
SHIE, Li-Yi <lyshie@tn.edu.tw>
2018-07-07 21:18:55 +0800

    Add font file for grub4dos

--- grub4dos/menu.lst.vhd
+++ grub4dos/menu.lst.vhd
@@ -1,8 +1,19 @@
+color blue/green yellow/red white/magenta white/magenta
+## menu border color
+color border=0xEEFFEE
+## set vbe mode
+graphicsmode -1 640:800 480:600 24:32 || graphicsmode -1 -1 -1 24:32
+font /unifont.hex.gz
+
 timeout 5
 default 0
 
 title Windows 10 (Native VHD Boot)
-find --set-root --ignore-floppies --ignore-cd /sig_intel_ssd
-dd if=()/disk_r.vhd of=()/disk.vhd
-find --set-root --ignore-floppies --ignore-cd /sig_intel_ssd
+find --set-root --ignore-floppies --ignore-cd /sig_ntfs
 chainloader /bootmgr
+
+title reboot (重新開機)
+reboot
+
+title halt (關機)
+halt
