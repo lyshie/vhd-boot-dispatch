a46da363b9c274727f2fbc409cf5a3e8c6f4ac4b
root <root@arch.chps.tn.edu.tw>
2018-07-17 17:49:56 +0800

    Clean files

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-su tc -c "tce-load -i /cde/optional/Xlibs.tcz"
-for z in $(cat /cde/onboot.lst); do             
-    su tc -c "tce-load -i /cde/optional/${z}"
-done                    
