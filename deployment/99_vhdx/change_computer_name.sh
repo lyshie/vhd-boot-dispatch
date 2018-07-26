d80ac0ba1f850c6a12518c36b583f0a06cf3aaeb
SHIE, Li-Yi <lyshie@tn.edu.tw>
2018-07-26 21:03:31 +0800

    Update scripts

--- deployment/99_vhdx/change_computer_name.sh
+++ deployment/99_vhdx/change_computer_name.sh
@@ -2,13 +2,17 @@
 
 # Forked from: https://gist.github.com/timnew/2373475
 
-declare ComputerName=$( ipconfig -all 2>&1 | awk -F: '/IPv4/ { print $2 }' | grep -oP "\d+\.\d+\.\d+\.\d+" | awk -F. '{ printf "PC-%s-%s", $3, $4 }' )
+export LC_ALL="C"
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Computername\Computername"       -v "Computername"          -t REG_SZ -d "${ComputerName}" -f
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Computername\ActiveComputername" -v "Computername"          -t REG_SZ -d "${ComputerName}" -f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"               -v "Hostname"              -t REG_SZ -d "${ComputerName}" -f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"               -v "NV Hostname"           -t REG_SZ -d "${ComputerName}" -f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"            -v "AltDefaultDomainName"  -t REG_SZ -d "${ComputerName}" -f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"            -v "DefaultDomainName"     -t REG_SZ -d "${ComputerName}" -f
+declare ComputerName=$( ipconfig -all 2>&1 | awk -F: '/IPv4/ { print $2 }' | head -n 1 | grep -oP "\d+\.\d+\.\d+\.\d+" | awk -F. '{ printf "PC-%s-%s", $3, $4 }' )
+
+if [ -n "${ComputerName}" ]; then
+    reg add "HKLM\SYSTEM\CurrentControlSet\Control\Computername\Computername"       -v "Computername"          -t REG_SZ -d "${ComputerName}" -f
+    reg add "HKLM\SYSTEM\CurrentControlSet\Control\Computername\ActiveComputername" -v "Computername"          -t REG_SZ -d "${ComputerName}" -f
+    reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"               -v "Hostname"              -t REG_SZ -d "${ComputerName}" -f
+    reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"               -v "NV Hostname"           -t REG_SZ -d "${ComputerName}" -f
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"            -v "AltDefaultDomainName"  -t REG_SZ -d "${ComputerName}" -f
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"            -v "DefaultDomainName"     -t REG_SZ -d "${ComputerName}" -f
+fi
 
 #Set-ItemProperty -path "HKU:\.Default\Software\Microsoft\Windows Media\WMSDK\General" -name "Computername" -value $ComputerName
