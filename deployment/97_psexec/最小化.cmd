a8eb5386a2935d373d6af016b022ccb51e7841c9
SHIE, Li-Yi <lyshie@tn.edu.tw>
2019-08-27 12:47:34 +0800

    加入 psexec 工具組

---
+++ deployment/97_psexec/最小化.cmd
@@ -0,0 +1,2 @@
+taskkill /F /IM cmd.exe
+powershell -command "(new-object -com shell.application).minimizeall();"
---
+++ deployment/97_psexec/還原佈景主題.cmd
@@ -0,0 +1,6 @@
+C:\Windows\Resources\Themes\aero.theme
+timeout /t 2
+C:\Windows\Resources\Themes\aero.theme
+timeout /t 2
+taskkill /F /IM systemsettings.exe
+powershell -command "(new-object -com shell.application).minimizeall();"
