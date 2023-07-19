a49025ccd13a1646ceee4d9c80d58f428963e263
root <root@arch.chps.tn.edu.tw>
2023-07-19 14:42:34 +0800

    Update pcroom2 MAC addresses

--- deployment/05_etherwake/boot_old.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#### 163.26.69.32
-etherwake -i enp2s0f0 -b EC:A8:6B:A5:F5:67
-sleep 1
--- deployment/05_etherwake/boot_teacher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#### 163.26.69.31
-etherwake -i enp2s0f0 -b C0:3F:D5:B7:1C:B1
-sleep 1
-
-#### 163.26.69.32
-etherwake -i enp2s0f0 -b EC:A8:6B:A5:F5:67
-sleep 1
