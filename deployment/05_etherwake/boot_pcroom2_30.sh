e858ace7de8b9a7c3d988dfa3033f4f4a081038d
root <root@arch.chps.tn.edu.tw>
2023-07-19 15:44:19 +0800

    Fix typo

---
+++ deployment/05_etherwake/boot_pcroom2_30.sh
@@ -0,0 +1,133 @@
+#!/bin/sh
+
+#### PC_41
+etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:87
+sleep 1
+
+#### PC_42
+etherwake -i enp2s0f0 -b C8:7F:54:EB:07:98
+sleep 1
+
+#### PC_43
+etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:80
+sleep 1
+
+#### PC_44
+etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:44
+sleep 1
+
+#### PC_45
+etherwake -i enp2s0f0 -b C8:7F:54:EB:09:D0
+sleep 1
+
+#### PC_46
+etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:66
+sleep 1
+
+#### PC_47
+etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:83
+sleep 1
+
+#### PC_48
+etherwake -i enp2s0f0 -b C8:7F:54:EB:12:9A
+sleep 1
+
+#### PC_49
+etherwake -i enp2s0f0 -b C8:7F:54:EB:06:BF
+sleep 1
+
+#### PC_50
+etherwake -i enp2s0f0 -b C8:7F:54:EB:07:1E
+sleep 1
+
+#### PC_51
+etherwake -i enp2s0f0 -b C8:7F:54:EB:07:C0
+sleep 1
+
+#### PC_52
+etherwake -i enp2s0f0 -b C8:7F:54:EB:05:F6
+sleep 1
+
+#### PC_53
+etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:75
+sleep 1
+
+#### PC_54
+etherwake -i enp2s0f0 -b C8:7F:54:EB:08:2B
+sleep 1
+
+#### PC_55
+etherwake -i enp2s0f0 -b C8:7F:54:EB:04:9E
+sleep 1
+
+#### PC_56
+etherwake -i enp2s0f0 -b C8:7F:54:EB:13:FD
+sleep 1
+
+#### PC_57
+etherwake -i enp2s0f0 -b C8:7F:54:EB:15:87
+sleep 1
+
+#### PC_58
+etherwake -i enp2s0f0 -b C8:7F:54:EB:15:B7
+sleep 1
+
+#### PC_59
+etherwake -i enp2s0f0 -b C8:7F:54:EB:04:92
+sleep 1
+
+#### PC_60
+etherwake -i enp2s0f0 -b C8:7F:54:EB:15:AD
+sleep 1
+
+#### PC_61
+etherwake -i enp2s0f0 -b C8:7F:54:EB:09:D9
+sleep 1
+
+#### PC_62
+etherwake -i enp2s0f0 -b C8:7F:54:EB:06:CD
+sleep 1
+
+#### PC_63
+etherwake -i enp2s0f0 -b C8:7F:54:EB:06:15
+sleep 1
+
+#### PC_64
+etherwake -i enp2s0f0 -b C8:7F:54:EB:15:50
+sleep 1
+
+#### PC_65
+etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:0C
+sleep 1
+
+#### PC_66
+etherwake -i enp2s0f0 -b C8:7F:54:EB:07:36
+sleep 1
+
+#### PC_67
+etherwake -i enp2s0f0 -b C8:7F:54:EB:06:B2
+sleep 1
+
+#### PC_68
+etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:5B
+sleep 1
+
+#### PC_69
+etherwake -i enp2s0f0 -b C8:7F:54:EB:09:0D
+sleep 1
+
+#### PC_70
+etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:11
+sleep 1
+
+#### PC_71
+#etherwake -i enp2s0f0 -b C8:7F:54:EB:07:18
+#sleep 1
+
+#### PC_72
+#etherwake -i enp2s0f0 -b C8:7F:54:EB:0A:15
+#sleep 1
+
+#### PC_73
+#etherwake -i enp2s0f0 -b 08:00:27:59:87:C0
+#sleep 1
